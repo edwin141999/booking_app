ad0b0166ed4c101b679ba14410f0d325f0643c50
edwin141999 <183433@ids.upchiapas.edu.mx>
2023-01-02 23:18:23 -0600

    cambio de staful a staless

--- lib/screen/welcome/welcome_screen.dart
+++ lib/screen/welcome/welcome_screen.dart
@@ -2,14 +2,9 @@ import 'package:booking_app/screen/welcome/log_in_email.dart';
 import 'package:booking_app/screen/welcome/onboarding.dart';
 import 'package:flutter/material.dart';
 
-class WelcomeScreen extends StatefulWidget {
+class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({Key? key}) : super(key: key);
 
-  @override
-  State<WelcomeScreen> createState() => _WelcomeScreenState();
-}
-
-class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
     final PageController controller = PageController(initialPage: 0);
