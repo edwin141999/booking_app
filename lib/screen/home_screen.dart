8a4564943bd0afc8c8369f060d6691772e12881c
edwin141999 <gendevilman@gmail.com>
2023-02-27 12:01:24 -0600

    packagePageView, por hacer modal de check availability

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,10 +1,7 @@
-import 'package:booking_app/models/card_package_model.dart';
 import 'package:booking_app/styles/colors/colors_view.dart';
 import 'package:booking_app/styles/fonts/fonts_view.dart';
-import 'package:booking_app/widgets/card_package.dart';
 import 'package:booking_app/widgets/container_image.dart';
-import 'package:booking_app/widgets/packages.dart';
-import 'package:booking_app/widgets/top_packages.dart';
+import 'package:booking_app/widgets/package_pageview.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -18,8 +15,8 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   var categories = ['Packages', 'Flights', 'Places', 'Hotels'];
   int index = 0;
-  final PageController _pageController = PageController(initialPage: 0);
-  int activePage = 3;
+  // final PageController _pageController = PageController(initialPage: 0);
+  // int activePage = 3;
   final ScrollController _scrollController =
       ScrollController(initialScrollOffset: 0);
   int activeScroll = 0;
@@ -45,156 +42,58 @@ class _HomeScreenState extends State<HomeScreen> {
         elevation: 0,
         backgroundColor: Colors.white,
         automaticallyImplyLeading: false,
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(50),
+          child: SizedBox(
+            width: double.infinity,
+            height: MediaQuery.of(context).size.height * .055,
+            child: ListView.separated(
+              itemCount: categories.length,
+              scrollDirection: Axis.horizontal,
+              controller: _scrollController,
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              separatorBuilder: (BuildContext context, int index) {
+                return const SizedBox(width: 32);
+              },
+              itemBuilder: (BuildContext context, int index) {
+                return Column(
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          activeScroll = index;
+                        });
+                      },
+                      child: Text(categories[index],
+                          style: FontSelect.kSubtitle16.copyWith(
+                            color: activeScroll == index
+                                ? ColorSelect.kColorSecondary
+                                : ColorSelect.kTextSecondary,
+                          )),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.all(3),
+                      width: 20,
+                      height: 7,
+                      decoration: BoxDecoration(
+                        color: activeScroll == index
+                            ? ColorSelect.kColorSecondary
+                            : Colors.transparent,
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(50)),
+                      ),
+                    ),
+                  ],
+                );
+              },
+            ),
+          ),
+        ),
       ),
       backgroundColor: Colors.white,
       body: SafeArea(
         minimum: const EdgeInsets.symmetric(horizontal: 20),
-        child: SingleChildScrollView(
-          physics: const BouncingScrollPhysics(),
-          child: Column(
-            children: [
-              const SizedBox(height: 20),
-              SizedBox(
-                width: double.infinity,
-                height: MediaQuery.of(context).size.height * .055,
-                child: ListView.separated(
-                    itemCount: categories.length,
-                    scrollDirection: Axis.horizontal,
-                    controller: _scrollController,
-                    padding: const EdgeInsets.symmetric(horizontal: 10),
-                    separatorBuilder: (BuildContext context, int index) {
-                      return const SizedBox(width: 32);
-                    },
-                    itemBuilder: (BuildContext context, int index) {
-                      return Column(
-                        children: [
-                          GestureDetector(
-                            onTap: () {
-                              setState(() {
-                                activeScroll = index;
-                              });
-                            },
-                            child: Text(categories[index],
-                                style: FontSelect.kSubtitle16.copyWith(
-                                  color: activeScroll == index
-                                      ? ColorSelect.kColorSecondary
-                                      : ColorSelect.kTextSecondary,
-                                )),
-                          ),
-                          Container(
-                            margin: const EdgeInsets.all(3),
-                            width: 20,
-                            height: 7,
-                            decoration: BoxDecoration(
-                              color: activeScroll == index
-                                  ? ColorSelect.kColorSecondary
-                                  : Colors.transparent,
-                              borderRadius:
-                                  const BorderRadius.all(Radius.circular(50)),
-                            ),
-                          ),
-                        ],
-                      );
-                    }),
-              ),
-              // PACKAGES
-              const SizedBox(height: 20),
-              Row(
-                children: [
-                  const Text('101 Packages', style: FontSelect.kSubtitle16),
-                  const Spacer(),
-                  Text('See all',
-                      style: FontSelect.kSubtitle18.copyWith(
-                          color: Colors.blue, fontWeight: FontWeight.w700)),
-                ],
-              ),
-              const SizedBox(height: 20),
-              const Packages(),
-              // POPULAR PACKAGES
-              const SizedBox(height: 20),
-              Row(
-                children: [
-                  const Text('Popular Packages', style: FontSelect.kTitle22),
-                  const Spacer(),
-                  TextButton(
-                    style: TextButton.styleFrom(
-                        padding: const EdgeInsets.all(0),
-                        elevation: 0,
-                        side: const BorderSide(color: Colors.transparent)),
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/popular',
-                          arguments: userData);
-                    },
-                    child: Text('See all',
-                        style: FontSelect.kSubtitle18.copyWith(
-                            color: Colors.blue, fontWeight: FontWeight.w700)),
-                  )
-                ],
-              ),
-              const SizedBox(height: 10),
-              SizedBox(
-                width: double.infinity,
-                height: 200,
-                child: PageView.builder(
-                  physics: const BouncingScrollPhysics(),
-                  itemCount: reversePackage.length,
-                  pageSnapping: true,
-                  controller: _pageController,
-                  onPageChanged: (int index) {
-                    setState(() {
-                      activePage = index % 3;
-                    });
-                  },
-                  itemBuilder: (BuildContext context, int index) {
-                    double margin = index == activePage ? 0 : 10;
-                    return AnimatedContainer(
-                      duration: const Duration(milliseconds: 500),
-                      curve: Curves.easeOutCubic,
-                      margin: EdgeInsets.symmetric(horizontal: margin),
-                      child: CardPackage(
-                        width: double.infinity,
-                        description: '2 days 3 night full package',
-                        save: true,
-                        image: reversePackage[index].image,
-                        place: reversePackage[index].place,
-                      ),
-                    );
-                  },
-                ),
-              ),
-              const SizedBox(height: 10),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: List.generate(reversePackage.length, (index) {
-                  return Container(
-                    margin: const EdgeInsets.all(3),
-                    width: activePage == index ? 30 : 10,
-                    height: 5,
-                    decoration: BoxDecoration(
-                      color:
-                          activePage == index ? Colors.blue : Colors.grey[200],
-                      borderRadius: BorderRadius.circular(50),
-                    ),
-                  );
-                }),
-              ),
-              // TOP PACKAGES
-              const SizedBox(height: 20),
-              Row(
-                children: [
-                  const Text('Top Packages', style: FontSelect.kTitle22),
-                  const Spacer(),
-                  Text('See all',
-                      style: FontSelect.kSubtitle18.copyWith(
-                          color: Colors.blue, fontWeight: FontWeight.w700)),
-                ],
-              ),
-              const SizedBox(height: 20),
-              const TopPackages(),
-              const SizedBox(height: 30)
-            ],
-          ),
-        ),
+        child: PackagePageView(userData: userData),
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const [
---
+++ lib/widgets/package_pageview.dart
@@ -0,0 +1,139 @@
+import 'package:booking_app/models/card_package_model.dart';
+import 'package:booking_app/styles/fonts/fonts_view.dart';
+import 'package:booking_app/widgets/card_package.dart';
+import 'package:booking_app/widgets/packages.dart';
+import 'package:booking_app/widgets/top_packages.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class PackagePageView extends StatefulWidget {
+  const PackagePageView({
+    Key? key,
+    required this.userData,
+    // required PageController pageController,
+    // required this.activePage
+  }) : super(key: key);
+
+  final UserCredential userData;
+  // final PageController _pageController;
+  // final int activePage;
+
+  @override
+  State<PackagePageView> createState() => _PackagePageViewState();
+}
+
+class _PackagePageViewState extends State<PackagePageView> {
+  final PageController _pageController = PageController(initialPage: 0);
+  int activePage = 3;
+
+  @override
+  void initState() {
+    activePage = 3;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      physics: const BouncingScrollPhysics(),
+      child: Column(
+        children: [
+          // PACKAGES
+          const SizedBox(height: 20),
+          Row(
+            children: [
+              const Text('101 Packages', style: FontSelect.kSubtitle16),
+              const Spacer(),
+              Text('See all',
+                  style: FontSelect.kSubtitle18.copyWith(
+                      color: Colors.blue, fontWeight: FontWeight.w700)),
+            ],
+          ),
+          const SizedBox(height: 20),
+          const Packages(),
+          // POPULAR PACKAGES
+          const SizedBox(height: 20),
+          Row(
+            children: [
+              const Text('Popular Packages', style: FontSelect.kTitle22),
+              const Spacer(),
+              TextButton(
+                style: TextButton.styleFrom(
+                    padding: const EdgeInsets.all(0),
+                    elevation: 0,
+                    side: const BorderSide(color: Colors.transparent)),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/popular',
+                      arguments: widget.userData);
+                },
+                child: Text('See all',
+                    style: FontSelect.kSubtitle18.copyWith(
+                        color: Colors.blue, fontWeight: FontWeight.w700)),
+              )
+            ],
+          ),
+          const SizedBox(height: 10),
+          SizedBox(
+            width: double.infinity,
+            height: 200,
+            child: PageView.builder(
+              physics: const BouncingScrollPhysics(),
+              itemCount: reversePackage.length,
+              pageSnapping: true,
+              controller: _pageController,
+              onPageChanged: (int index) {
+                setState(() {
+                  activePage = index % 3;
+                });
+              },
+              itemBuilder: (BuildContext context, int index) {
+                double margin = index == activePage ? 0 : 10;
+                return AnimatedContainer(
+                  duration: const Duration(milliseconds: 500),
+                  curve: Curves.easeOutCubic,
+                  margin: EdgeInsets.symmetric(horizontal: margin),
+                  child: CardPackage(
+                    width: double.infinity,
+                    description: '2 days 3 night full package',
+                    save: true,
+                    image: reversePackage[index].image,
+                    place: reversePackage[index].place,
+                  ),
+                );
+              },
+            ),
+          ),
+          const SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: List.generate(reversePackage.length, (index) {
+              return Container(
+                margin: const EdgeInsets.all(3),
+                width: activePage == index ? 30 : 10,
+                height: 5,
+                decoration: BoxDecoration(
+                  color: activePage == index ? Colors.blue : Colors.grey[200],
+                  borderRadius: BorderRadius.circular(50),
+                ),
+              );
+            }),
+          ),
+          // TOP PACKAGES
+          const SizedBox(height: 20),
+          Row(
+            children: [
+              const Text('Top Packages', style: FontSelect.kTitle22),
+              const Spacer(),
+              Text('See all',
+                  style: FontSelect.kSubtitle18.copyWith(
+                      color: Colors.blue, fontWeight: FontWeight.w700)),
+            ],
+          ),
+          const SizedBox(height: 20),
+          const TopPackages(),
+          const SizedBox(height: 30)
+        ],
+      ),
+    );
+  }
+}
